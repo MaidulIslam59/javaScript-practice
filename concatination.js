226edff16f92317c53195235945f51d48f3846f6
MaidulIslam59 <maiduluapcse@gmail.com>
2024-07-31 13:11:39 +0600

    learn javascript concatination and conversion

---
+++ concatination.js
@@ -0,0 +1,8 @@
+var firstName = 'Lionel';
+var lastName = 'Messi';
+var address = 'Miami, USA';
+var jerseyNumber = 'Ten';
+
+
+var fullName = firstName + ' ' + lastName;
+console.log(fullName,'\n', address, '\n',jerseyNumber);
---
+++ conversion.js
@@ -0,0 +1,20 @@
+var price1 = 32;
+var price2 = '40';
+
+var totalPrice = price1 + price2;
+console.log(totalPrice);
+
+//convert string to number
+var price1 = 32;
+var price2 = '40';
+var price3 = '3.11'
+
+var price2Number = parseInt(price2);
+var price3Number = parseFloat(price3);
+var totalPrice = price1 + price2Number;
+var price = price2Number + price3Number;
+
+console.log(price3Number);
+console.log(price2Number);
+console.log(totalPrice);
+console.log(price);
