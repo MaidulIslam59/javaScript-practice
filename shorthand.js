69f64cf60200d0831f376535edc91501a03a3349
MaidulIslam59 <maiduluapcse@gmail.com>
2024-07-31 12:32:11 +0600

    learn javascript operations_shorthand  addition,subtraction,multiplication & division

---
+++ shorthand.js
@@ -0,0 +1,53 @@
+var price1 = 33;
+var price2 = 20;
+var price3 = 55;
+
+
+//update value/set new value
+price1 = 36;
+
+//adding some value to the exiting one
+
+//price1 = price1 + 10;
+
+//shorthand addition
+price1 += 10;
+
+console.log(price1);
+
+// subtraction some value to the exiting one
+//price2 = price2 - 5;
+//shorthand subtraction
+price2 -= 5;
+console.log(price2)
+
+//multiplication
+//price1 = price1 * price2;
+//shorthand
+price1 *= price2;
+console.log(price1);
+
+//division
+// price3 = price3 / 5;
+//shorthand
+price3 /= 5;
+console.log(price3)
+
+
+
+//learn additonal shorthand
+
+var count = 4;
+//add
+// count = count + 1;
+
+count++;
+console.log(count);
+
+//subtraction
+var count = 4;
+//add
+// count = count - 1;
+
+count--;
+console.log(count);
