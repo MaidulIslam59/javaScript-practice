082dddd00070e60a793b620e16d3285ab1c87dd8
MaidulIslam59 <maiduluapcse@gmail.com>
2024-07-31 11:31:23 +0600

    learn javascript operations- addition,subtraction,multiplication & division

--- operation.js
+++ operation.js
@@ -1,7 +1,28 @@
 var mangoPrice = 200;
 var applePrice = 230;
 
+//addition and subtraction
 var totalPrice = (mangoPrice + applePrice);
 console.log(totalPrice);
+
+var priceDifference = mangoPrice - applePrice;
+console.log(priceDifference);
 //console.log(mangoPrice, applePrice);
 //console.log(mangoPrice + applePrice);
+
+//multiplication 
+var ballPrice = 400;
+var quantity = 8;
+
+var totalPrice = ballPrice * quantity;
+
+console.log(totalPrice);
+
+//division
+
+var amount = 5000;
+var employee = 9;
+
+var eachEmployee = amount / employee;
+
+console.log(eachEmployee);
