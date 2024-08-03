4d2f1d50058d459efa00cd0178727bcb4d738fba
MaidulIslam59 <maiduluapcse@gmail.com>
2024-08-03 23:29:26 +0600

    Create two folder JavaScript_basic and practice problem__ and practice 4 problems

--- practice_problems/pp4.js
+++ practice_problems/pp4.js
@@ -3,3 +3,12 @@ var num2 = 5;
 
 var remainder = num1 % num2;
 console.log(remainder);
+
+var num3 = 120;
+var num4 = 60;
+var num5 = 23;
+
+var total = num3 + num4;
+
+var remainder = total % num5;
+console.log(remainder);
