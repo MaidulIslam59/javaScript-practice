7f34329e69adb650d5d9b50b09e4aa205781f543
MaidulIslam59 <maiduluapcse@gmail.com>
2024-08-06 22:34:11 +0600

    practice some comparison

--- js-array-conditionals/comparison.js
+++ js-array-conditionals/comparison.js
@@ -1,12 +1,40 @@
-//less than 
+//less than
 // console.log(3 < 4);
 // console.log(4 < 3);
+
 //greater than
 // console.log(4 > 3);
 // console.log(3 > 4);
+
 //equal
-console.log(4 == 3);
-console.log(4 == 4);
+// console.log(4 == 3);
+// console.log(4 == 4);
+
 //not equal
-console.log(4 != 3);
-console.log(4 != 4);
+// console.log(4 != 3);
+// console.log(4 != 4);
+
+//less than or equal
+// console.log(10 <= 20);
+// console.log(20 <= 10);
+
+
+//greater than or equal
+// console.log(16 >= 6);
+// console.log(6 >= 16);
+
+
+var num1 = 35;
+var num2 = 55;
+
+console.log(num1 > num2);
+
+console.log(num1 < num2);
+
+console.log(num1 >= num2);
+
+console.log(num1 <= num2);
+
+console.log(num1 != num2);
+
+console.log(num1 == num2);
