c056971dcfc7f49c0397d97a83713562597d8da3
MaidulIslam59 <maiduluapcse@gmail.com>
2024-08-06 15:21:11 +0600

    practice some comparison

--- js-array-conditionals/comparison.js
+++ js-array-conditionals/comparison.js
@@ -1,2 +1,12 @@
-console.log(3 < 4);
-console.log(4 < 3);
+//less than 
+// console.log(3 < 4);
+// console.log(4 < 3);
+//greater than
+// console.log(4 > 3);
+// console.log(3 > 4);
+//equal
+console.log(4 == 3);
+console.log(4 == 4);
+//not equal
+console.log(4 != 3);
+console.log(4 != 4);
