324ba553d927f4303ab605aabff0a1792574b099
MaidulIslam59 <maiduluapcse@gmail.com>
2024-08-07 12:41:44 +0600

    create conditional.js and multiple-conditions.js

---
+++ js-array-conditionals/conditional.js
@@ -0,0 +1,16 @@
+var bikePrice = 350000;
+var myBudget = 240000;
+
+if (bikePrice < myBudget) {
+    console.log("i will buy this bike");
+}
+else {
+    console.log("i won't buy this bike");
+}
+
+if (bikePrice > myBudget) {
+    console.log("i will buy this bike");
+}
+else {
+    console.log("i won't buy this bike");
+}
---
+++ js-array-conditionals/multiple-conditions.js
@@ -0,0 +1,36 @@
+// var isGraduate = true;
+// var salary = 20000;
+//when tow conditions is true
+// if (isGraduate == true && salary > 25000) {
+//     console.log("I am happy");
+// }
+// else {
+//     console.log("I am not happy");
+// }
+//when one conditions is true
+// if (isGraduate == true || salary > 25000) {
+//     console.log("I am happy");
+// }
+// else {
+//     console.log("I am not happy");
+// }
+
+var isGraduate = true;
+var salary = 30000;
+var car = 0;
+
+
+
+if (isGraduate == true && salary > 25000 && car>=1) {
+    console.log("I am happy");
+}
+else {
+    console.log("I am not happy");
+}
+
+if (isGraduate == true && salary > 25000 || car>=1) {
+    console.log("I am happy");
+}
+else {
+    console.log("I am not happy");
+}
