59948a243064d7a90d76831cb99c02555abd0ac1
MaidulIslam59 <maiduluapcse@gmail.com>
2024-08-09 23:34:22 +0600

    practice return

--- js-function-object/return.js
+++ js-function-object/return.js
@@ -7,3 +7,15 @@ function bringBurger(money) {
 var myBudget = 320;
 var burgers = bringBurger(myBudget);
 console.log("Eating burger: ", burgers);
+
+
+//another practice
+function bringFood(money) {
+    var foodPrice = 120;
+    var quantity = money / foodPrice;
+    return quantity;
+}
+
+var myBudget = 320;
+var foods = bringFood(myBudget);
+console.log("Eating food: ", foods);
